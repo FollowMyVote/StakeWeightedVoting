5d063a172d6e6bc621b953e2784f2101befec0c5
Nathan Hourt <nat.hourt@gmail.com>
2015-11-16 10:36:11 -0500

    Add missing header

---
+++ VotingApp/wrappers/Converters.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 Follow My Vote, Inc.
+ * This file is part of The Follow My Vote Stake-Weighted Voting Application ("SWV").
+ *
+ * SWV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SWV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QByteArray>
+#include <QJsonObject>
+
+#include <backend.capnp.h>
+
+QByteArray convertBlob(capnp::Data::Reader data) {
+    return QByteArray(reinterpret_cast<const char*>(data.begin()), static_cast<signed>(data.size()));
+}
+
+QJsonObject convertListedContest(ContestGenerator::ListedContest::Reader contest) {
+    return {{"contestId", QString(convertBlob(contest.getContestId()).toHex())},
+            {"votingStake", qint64(contest.getVotingStake())},
+            {"tracksLiveResults", contest.getTracksLiveResults()}};
+}
