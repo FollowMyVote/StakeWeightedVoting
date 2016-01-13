4d82f60cc09e498fd2e16b188718c98884cf2a50
Nathan Hourt <nat.hourt@gmail.com>
2016-01-13 16:32:24 -0600

    Progress #19: Initial work: sponsorship in PurchaseContestRequestWrapper
    Also, refactor all inline methods to the cpp file, more cleanup with
    macros

--- VotingApp/wrappers/PurchaseContestRequest.cpp
+++ VotingApp/wrappers/PurchaseContestRequest.cpp
@@ -15,13 +15,25 @@
 
 // Macro to generate getter and setter for enum properties
 #define ENUM_GETTER_SETTER(property, upperProperty) \
-    upperProperty::Type swv::PurchaseContestRequestWrapper::property() const { \
+    upperProperty::Type PurchaseContestRequestWrapper::property() const { \
         return static_cast<upperProperty::Type>(request.asReader().getRequest().get ## upperProperty ()); \
     } \
-    void swv::PurchaseContestRequestWrapper::set ## upperProperty(upperProperty::Type property) { \
+    void PurchaseContestRequestWrapper::set ## upperProperty(upperProperty::Type property) { \
         if (property == this->property()) \
             return; \
         request.getRequest().set ## upperProperty (static_cast<::ContestCreator::upperProperty ## s>(property)); \
+        emit property ## Changed(property); \
+    }
+
+// Macro to generate getter and setter for properties which require no special conversion steps
+#define SIMPLE_GETTER_SETTER(property, upperProperty, type, requestField) \
+    type PurchaseContestRequestWrapper::property() const { \
+        return request.asReader().getRequest().get ## requestField(); \
+    } \
+    void PurchaseContestRequestWrapper::set ## upperProperty(type property) { \
+        if (property == this->property()) \
+            return; \
+        request.getRequest().set ## requestField(property); \
         emit property ## Changed(property); \
     }
 
@@ -37,4 +49,13 @@ ENUM_GETTER_SETTER(contestType, ContestType)
 ENUM_GETTER_SETTER(tallyAlgorithm, TallyAlgorithm)
 TEXT_GETTER_SETTER(name, Name, ContestName)
 TEXT_GETTER_SETTER(description, Description, ContestDescription)
+SIMPLE_GETTER_SETTER(weightCoin, WeightCoin, quint64, WeightCoin)
+SIMPLE_GETTER_SETTER(expiration, Expiration, qint64, ContestExpiration)
+
+bool PurchaseContestRequestWrapper::sponsorshipEnabled() const {
+    return request.asReader().getRequest().getSponsorship().isOptions();
+}
+void PurchaseContestRequestWrapper::disableSponsorship() {
+    request.getRequest().initSponsorship().setNoSponsorship();
+}
 } // namespace swv
