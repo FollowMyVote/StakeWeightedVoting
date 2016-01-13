6d25dba9c2f51906e96e6180e87e3ff606cba80b
Nathan Hourt <nat.hourt@gmail.com>
2016-01-13 16:02:51 -0600

    Progress #19: Add enum types/properties to PurchaseContestRequestWrapper

--- VotingApp/wrappers/PurchaseContestRequest.cpp
+++ VotingApp/wrappers/PurchaseContestRequest.cpp
@@ -1,21 +1,28 @@
 #include "PurchaseContestRequest.hpp"
 #include "Converters.hpp"
 
-#define TEXT_GETTER(requestField) \
-    return convertText(request.asReader().getRequest().get ## requestField ())
-
-#define TEXT_SETTER(property, requestField) \
-    if (property == this->property()) \
-    return; \
-    convertText(request.getRequest().get ## requestField (), property); \
-    emit property ## Changed(property)
-
+// Macro to generate getter and setter for text properties
 #define TEXT_GETTER_SETTER(property, upperProperty, requestField) \
     QString PurchaseContestRequestWrapper::property() const { \
-    TEXT_GETTER(requestField); \
+        return convertText(request.asReader().getRequest().get ## requestField ()); \
     } \
     void PurchaseContestRequestWrapper::set ## upperProperty(QString property) { \
-    TEXT_SETTER(property, requestField); \
+        if (property == this->property()) \
+        return; \
+        convertText(request.getRequest().get ## requestField (), property); \
+        emit property ## Changed(property); \
+    }
+
+// Macro to generate getter and setter for enum properties
+#define ENUM_GETTER_SETTER(property, upperProperty) \
+    upperProperty::Type swv::PurchaseContestRequestWrapper::property() const { \
+        return static_cast<upperProperty::Type>(request.asReader().getRequest().get ## upperProperty ()); \
+    } \
+    void swv::PurchaseContestRequestWrapper::set ## upperProperty(upperProperty::Type property) { \
+        if (property == this->property()) \
+            return; \
+        request.getRequest().set ## upperProperty (static_cast<::ContestCreator::upperProperty ## s>(property)); \
+        emit property ## Changed(property); \
     }
 
 namespace swv {
@@ -26,6 +33,8 @@ PurchaseContestRequestWrapper::PurchaseContestRequestWrapper(PurchaseRequest&& r
 {
 }
 
+ENUM_GETTER_SETTER(contestType, ContestType)
+ENUM_GETTER_SETTER(tallyAlgorithm, TallyAlgorithm)
 TEXT_GETTER_SETTER(name, Name, ContestName)
 TEXT_GETTER_SETTER(description, Description, ContestDescription)
 } // namespace swv
