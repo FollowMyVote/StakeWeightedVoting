82c0a5ebf857334ffa6e0eb18f85e1858e8dd228
Nathan Hourt <nathan@followmyvote.com>
2016-03-22 17:30:50 -0500

    Make GrapheneBackend subproject into a graphene full node

---
+++ GrapheneBackend/BackendPlugin.hpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Follow My Vote, Inc.
+ * This file is part of The Follow My Vote Stake-Weighted Voting Application ("SWV").
+ *
+ * SWV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SWV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef BACKENDPLUGIN_HPP
+#define BACKENDPLUGIN_HPP
+
+#include <graphene/app/plugin.hpp>
+
+namespace swv {
+
+class BackendPlugin : public graphene::app::plugin
+{
+public:
+    BackendPlugin();
+
+    // abstract_plugin interface
+    virtual std::string plugin_name() const override;
+    virtual void plugin_initialize(const boost::program_options::variables_map& options) override;
+    virtual void plugin_startup() override;
+    virtual void plugin_shutdown() override;
+    virtual void plugin_set_program_options(boost::program_options::options_description& command_line_options,
+                                            boost::program_options::options_description& config_file_options) override;
+};
+
+} // namespace swv
+
+#endif // BACKENDPLUGIN_HPP
--- qbs/modules/graphene/graphene.qbs
+++ qbs/modules/graphene/graphene.qbs
@@ -6,6 +6,29 @@ Module {
     property string graphenePath: qbs.getEnv("GRAPHENE_PATH")
     property bool found: File.exists(graphenePath+"/lib/libgraphene_app.a")
     cpp.includePaths: graphenePath+"/include"
-    cpp.libraryPaths: graphenePath+"/lib"
-    cpp.staticLibraries: ["fc", "graphene_app","graphene_chain"]
+    cpp.libraryPaths: [graphenePath+"/lib", graphenePath+"/lib/cryptonomex"]
+    cpp.staticLibraries: [
+        "boost_system",
+        "boost_filesystem",
+        "boost_program_options",
+        "boost_chrono",
+        "boost_date_time",
+        "boost_coroutine",
+        "boost_context",
+        "boost_thread",
+        "ssl",
+        "crypto",
+        "graphene_app",
+        "graphene_chain",
+        "graphene_utilities",
+        "graphene_net",
+        "graphene_time",
+        "graphene_egenesis_full",
+        "graphene_db",
+        "graphene_debug_witness",
+        "graphene_market_history",
+        "fc",
+        "secp256k1",
+        "z"
+    ]
 }
